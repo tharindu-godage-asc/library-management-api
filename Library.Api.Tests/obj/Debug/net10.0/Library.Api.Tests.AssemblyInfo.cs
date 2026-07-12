f93f1dfa0e4f55f1099c18bd21b4986ac18df746
TharinduGodage <TharinduGodage@ASC-LT-ENG-THAR>
2026-07-12 12:08:44 +0530

    Update assembly informational version for new build
    Updated AssemblyInformationalVersionAttribute in Library.Api.Tests to reflect a new build/commit. Synchronized related cache files to match the updated version information.

--- Library.Api.Tests/obj/Debug/net10.0/Library.Api.Tests.AssemblyInfo.cs
+++ Library.Api.Tests/obj/Debug/net10.0/Library.Api.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f43925c93fff5f48dc627727e277eaebad5d13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e51bf535958cba5e83d5119d3c4abf4d0c54d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
